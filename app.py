604bbc59e7ae65a9e68ec9223fceff718bc29c54
por314159ug <por.314.u.g@gmail.com>
2025-05-13 15:49:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/example')
+def example():
+    return "Another endpoint available!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
