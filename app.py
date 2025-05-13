147af7025203bdc863b04ba83b4f6e6e9d033ce4
por314159ug <por.314.u.g@gmail.com>
2025-05-13 16:18:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def hello():
 def example():
     return "Another endpoint available!"
 
+@app.route('/example-3')
+def example():
+    return "Third endpoint available!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
